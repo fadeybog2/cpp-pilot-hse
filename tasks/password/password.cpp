210365e93f4e47c71fa58df743bf788fa73bcab0
Fadey Bogomiakov <fadey2@gmail.com>
2022-01-21 20:42:19 +0300

    clang tidy

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,32 +1,32 @@
 #include "password.h"
 
 bool ValidatePassword(const std::string& password) {
-    const int MIN_LENGTH = 8;
-    const int MAX_LENGTH = 14;
-    const int MIN_ASCII = 33;
-    const int MAX_ASCII = 126;
-    bool HasUpper = false;
-    bool HasLower = false;
-    bool HasDigit = false;
-    bool HasOther = false;
-    if (password.length() < MIN_LENGTH || password.length() > MAX_LENGTH) {
+    const int min_length = 8;
+    const int max_length = 14;
+    const int min_ascii = 33;
+    const int max_ascii = 126;
+    bool has_upper = false;
+    bool has_lower = false;
+    bool has_digit = false;
+    bool has_other = false;
+    if (password.length() < min_length || password.length() > max_length) {
         return false;
     }
     for (auto c : password) {
         if (std::islower(c)) {
-            HasLower = true;
+            has_lower = true;
         } else if (std::isupper(c)) {
-            HasUpper = true;
+            has_upper = true;
         } else if (std::isdigit(c)) {
-            HasDigit = true;
+            has_digit = true;
         } else {
-            HasOther = true;
+            has_other = true;
         }
-        if (int(c) < MIN_ASCII || int(c) > MAX_ASCII) {
+        if (int(c) < min_ascii || int(c) > max_ascii) {
             return false;
         }
     }
-    if (HasUpper + HasLower + HasDigit + HasOther < 3) {
+    if (has_upper + has_lower + has_digit + has_other < 3) {
         return false;
     }
     return true;
