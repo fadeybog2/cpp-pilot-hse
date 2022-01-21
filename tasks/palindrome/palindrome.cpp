708a1c64b4bbf5e6f19f15e0e584ee625f249804
Fadey Bogomiakov <fadey2@gmail.com>
2022-01-21 20:36:31 +0300

    palindrome

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,20 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    int l = 0;
+    int r = str.length() - 1;
+    while (l <= r) {
+        while(str[l] == ' '){
+            l++;
+        }
+        while(str[r] == ' '){
+            r--;
+        }
+        if (str[l] != str[r]){
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 }
