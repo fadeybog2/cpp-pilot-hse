14e4dc9e21108cd0b4d9f68e8a7cbcfa85b0d5b2
Fadey Bogomiakov <fadey2@gmail.com>
2022-01-26 21:15:04 +0300

    Use list instead of vector

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -5,7 +5,7 @@
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
     std::vector<std::string> answer;
-    std::vector<std::string> stack;
+    std::list<std::string> stack;
     for (const auto& action : student_actions) {
         switch (action.side) {
             case Side::Bottom:
@@ -16,8 +16,9 @@ std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student
                 break;
         }
     }
+    std::vector<std::string> stack_vec(stack.begin(), stack.end());
     for (const auto& query : queries) {
-        answer.push_back(stack[query - 1]);
+        answer.push_back(stack_vec[query - 1]);
     }
     return answer;
 }
