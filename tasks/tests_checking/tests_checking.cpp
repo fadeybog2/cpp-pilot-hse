1541b6aa8ad375de5154c10e21d9c6bf936644f1
Fadey Bogomiakov <fadey2@gmail.com>
2022-01-26 17:20:16 +0300

    tests checking

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,6 +1,23 @@
 #include "tests_checking.h"
 
+#include <list>
+
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::vector<std::string> answer;
+    std::vector<std::string> stack;
+    for (const auto& action : student_actions) {
+        switch (action.side) {
+            case Side::Bottom:
+                stack.push_back(action.name);
+                break;
+            case Side::Top:
+                stack.insert(stack.begin(), action.name);
+                break;
+        }
+    }
+    for (const auto& query : queries) {
+        answer.push_back(stack[query - 1]);
+    }
+    return answer;
 }
