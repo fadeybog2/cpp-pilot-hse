b031ab283ba0a8fec9960d6e19acac73acb160fd
Fadey Bogomiakov <fadey2@gmail.com>
2022-01-27 08:28:26 +0300

    clang-tidy: added spaces

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -2,10 +2,10 @@
 
 #include <vector>
 
-int ScalarMultiplication(std::vector<int> vector1, std::vector<int> vector2){
+int ScalarMultiplication(std::vector<int> vector1, std::vector<int> vector2) {
     int answer = 0;
     for (size_t i = 0; i < vector1.size(); ++i) {
-        answer+=vector1[i]*vector2[i];
+        answer += vector1[i] * vector2[i];
     }
     return answer;
 }
@@ -14,14 +14,14 @@ std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
     std::vector<int> first_vec = vectors.front();
     std::vector<std::string> answer;
     int max_sc_mp = -1;
-    for (size_t i = 0; i < vectors.size(); ++i){
+    for (size_t i = 0; i < vectors.size(); ++i) {
         auto vec = vectors[i];
         int sc_mp = ScalarMultiplication(first_vec, vec);
-        if (sc_mp > max_sc_mp){
+        if (sc_mp > max_sc_mp) {
             max_sc_mp = sc_mp;
             answer.clear();
         }
-        if (sc_mp == max_sc_mp){
+        if (sc_mp == max_sc_mp) {
             answer.push_back(words[i]);
         }
     }
