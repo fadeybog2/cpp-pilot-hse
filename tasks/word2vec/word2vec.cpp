61da92b29b4d5ff51bd45456919d70f9adbd6f20
Fadey Bogomiakov <fadey2@gmail.com>
2022-01-28 10:28:15 +0300

    - std::inner_product - Константные ссылки - Минимальное значение int

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -1,25 +1,20 @@
 #include "word2vec.h"
 
 #include <vector>
+#include <numeric>
+#include <limits>
 
-int ScalarMultiplication(std::vector<int> vector1, std::vector<int> vector2) {
-    int answer = 0;
-    for (size_t i = 0; i < vector1.size(); ++i) {
-        answer += vector1[i] * vector2[i];
-    }
-    return answer;
-}
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
     if (vectors.empty()) {
         return std::vector<std::string>();
     }
-    std::vector<int> first_vec = vectors.front();
+    const auto& first_vec = vectors.front();
     std::vector<std::string> answer;
-    int max_sc_mp = -1;
+    int max_sc_mp = std::numeric_limits<int>::min();
     for (size_t i = 1; i < vectors.size(); ++i) {
-        auto vec = vectors[i];
-        int sc_mp = ScalarMultiplication(first_vec, vec);
+        const auto& vec = vectors[i];
+        int sc_mp = std::inner_product(first_vec.begin(), first_vec.end(), vec.begin(), 0);
         if (sc_mp > max_sc_mp) {
             max_sc_mp = sc_mp;
             answer.clear();
