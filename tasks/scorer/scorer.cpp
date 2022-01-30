adc57a04f0f355f80ca84d6d66f4fc59dc229c6a
Fadey Bogomiakov <fadey2@gmail.com>
2022-01-30 12:37:50 +0300

    Изменил логику работы, чтобы убрать сортировку. Поправил switch.

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -1,28 +1,25 @@
 #include "scorer.h"
 
-#include <algorithm>
+#include <unordered_map>
 
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     ScoreTable score_table;
-    auto sorted_events = events;
-    auto comp = [](const Event& lhs, const Event& rhs) { return lhs.time < rhs.time; };
-    std::sort(sorted_events.begin(), sorted_events.end(), comp);
-    for (size_t i = 0; i < sorted_events.size() && sorted_events[i].time <= score_time; ++i) {
-        const auto& event = sorted_events[i];
+    std::unordered_map<StudentName, std::unordered_map<TaskName, time_t>> tasks_time;
+    for (const auto& event : events) {
+        if (event.time > score_time || event.time < tasks_time[event.student_name][event.task_name]) {
+            continue;
+        }
         switch (event.event_type) {
             case EventType::CheckFailed:
-                score_table[event.student_name].erase(event.task_name);
-                break;
-            case EventType::CheckSuccess:
-                score_table[event.student_name].insert(event.task_name);
-                break;
             case EventType::MergeRequestOpen:
                 score_table[event.student_name].erase(event.task_name);
                 break;
+            case EventType::CheckSuccess:
             case EventType::MergeRequestClosed:
                 score_table[event.student_name].insert(event.task_name);
                 break;
         }
+        tasks_time[event.student_name][event.task_name] = event.time;
         if (score_table[event.student_name].empty()) {
             score_table.erase(event.student_name);
         }
