7d5c9af0c4f8aa4d54e281221a81a0f223bba942
Fadey Bogomiakov <fadey2@gmail.com>
2022-02-01 13:23:02 +0300

    Добавил свои алгоритмы хэша и сравнения для нечувствительности к регистру

--- tasks/search/search.cpp
+++ tasks/search/search.cpp
@@ -2,27 +2,43 @@
 
 #include <cctype>
 #include <unordered_map>
-#include <set>
+#include <unordered_set>
 #include <algorithm>
 #include <cmath>
-#include <cctype>
-#include <string>
+#include <functional>
+
+struct Hash {
+    size_t operator()(std::string_view str) const {
+        std::size_t hash = 0;
+        for (const auto& c : str) {
+            hash += std::hash<char>{}(std::tolower(c));
+        }
+        return hash;
+    }
+};
+
+struct IsEqual {
+    bool operator()(std::string_view lhs, std::string_view rhs) const {
+        return std::equal(lhs.begin(), lhs.end(), rhs.begin(),
+                          [](char l, char r) { return std::tolower(l) == std::tolower(r); });
+    }
+};
 
 struct RelevString {
     std::string_view str;
     std::vector<std::string_view> words;
-    std::unordered_map<std::string_view, double> tf;
+    std::unordered_map<std::string_view, double, Hash, IsEqual> tf;
     double relevance = 0;
 };
 
-void SplitText(const std::string_view& text, const std::string_view& lower_text, std::vector<RelevString>& result) {
+void SplitText(const std::string_view& text, std::vector<RelevString>& result) {
     size_t beg = 0;
     size_t str_beg = 0;
     bool is_word = std::isalpha(text[beg]);
     for (size_t end = 0; end < text.size(); ++end) {
         if (end == text.size() - 1) {
             if (is_word) {
-                result.back().words.push_back(lower_text.substr(beg, end - beg));
+                result.back().words.push_back(text.substr(beg, end - beg));
                 is_word = false;
                 beg = end;
             }
@@ -34,7 +50,7 @@ void SplitText(const std::string_view& text, const std::string_view& lower_text,
             }
         } else {
             if (is_word) {
-                result.back().words.push_back(lower_text.substr(beg, end - beg));
+                result.back().words.push_back(text.substr(beg, end - beg));
                 is_word = false;
                 beg = end;
             }
@@ -81,16 +97,8 @@ std::vector<std::string_view> Search(std::string_view text, std::string_view que
     std::vector<std::string_view> answer;
     std::vector<std::string_view> splitted_query;
     std::vector<RelevString> rel_strs(1);
-    std::string lower_text;
-    std::string lower_query;
-    for (const auto& c : text) {
-        lower_text.push_back(tolower(c));
-    }
-    for (const auto& c : query) {
-        lower_query.push_back(tolower(c));
-    }
-    SplitText(text, lower_text, rel_strs);
-    SplitQuery(lower_query, splitted_query);
+    SplitText(text, rel_strs);
+    SplitQuery(query, splitted_query);
 
     size_t not_empty_strings_number = rel_strs.size();
     for (const auto& rel_str : rel_strs) {
@@ -99,8 +107,8 @@ std::vector<std::string_view> Search(std::string_view text, std::string_view que
         }
     }
 
-    std::unordered_map<std::string_view, double> idf;
-    std::set<std::string_view> words_in_query(splitted_query.begin(), splitted_query.end());
+    std::unordered_map<std::string_view, double, Hash, IsEqual> idf;
+    std::unordered_set<std::string_view, Hash, IsEqual> words_in_query(splitted_query.begin(), splitted_query.end());
     for (auto& rel_str : rel_strs) {
         for (const auto& word : rel_str.words) {
             if (words_in_query.contains(word)) {
@@ -110,7 +118,7 @@ std::vector<std::string_view> Search(std::string_view text, std::string_view que
     }
 
     for (auto& rel_str : rel_strs) {
-        std::set<std::string_view> words_set(rel_str.words.begin(), rel_str.words.end());
+        std::unordered_set<std::string_view, Hash, IsEqual> words_set(rel_str.words.begin(), rel_str.words.end());
         for (const auto& word : words_in_query) {
             if (words_set.contains(word)) {
                 idf[word] += 1.0 / not_empty_strings_number;
@@ -118,9 +126,9 @@ std::vector<std::string_view> Search(std::string_view text, std::string_view que
         }
     }
     for (auto& rel_str : rel_strs) {
-        for (const auto& pair : rel_str.tf) {
-            if (idf[pair.first] != 0) {
-                rel_str.relevance += pair.second * std::log(1.0 / idf[pair.first]);
+        for (const auto& [word, tf] : rel_str.tf) {
+            if (idf[word] != 0) {
+                rel_str.relevance += tf * std::log(1.0 / idf[word]);
             }
         }
     }
