2cf568bb562b0922a0474390b21192e62121b111
Fadey Bogomiakov <fadey2@gmail.com>
2022-02-16 13:11:45 +0300

    zip

--- tasks/zip/zip.cpp
+++ tasks/zip/zip.cpp
@@ -1,1 +1,33 @@
 #include "zip.h"
+
+Zipped::Zipped(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end)
+    : a_begin_(a_begin), a_end_(a_end), b_begin_(b_begin), b_end_(b_end) {
+}
+
+Zipped::ZipIterator Zipped::begin() {
+    return ZipIterator(a_begin_, b_begin_);
+}
+
+Zipped::ZipIterator Zipped::end() {
+    return ZipIterator(a_end_, b_end_);
+}
+
+ZippedPair Zipped::ZipIterator::operator*() const {
+    return ZippedPair(*it_a_, *it_b_);
+}
+
+bool Zipped::ZipIterator::operator!=(const ZipIterator& other) {
+    return it_a_ != other.it_a_ && it_b_ != other.it_b_;
+}
+
+Zipped::ZipIterator::ZipIterator(Iterator it1, Iterator it2) : it_a_(it1), it_b_(it2) {
+}
+
+void Zipped::ZipIterator::operator++() {
+    ++it_a_;
+    ++it_b_;
+}
+
+Zipped Zip(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end) {
+    return Zipped(a_begin, a_end, b_begin, b_end);
+}
--- tasks/zip/zip.h
+++ tasks/zip/zip.h
@@ -4,9 +4,31 @@
 #include <string>
 
 using Value = std::string;
-using Iterator = std::forward_list<std::string>::const_iterator;
+using Iterator = std::forward_list<Value>::const_iterator;
 using ZippedPair = std::pair<const Value&, const Value&>;
 
-class Zipped;
+class Zipped {
+public:
+    Zipped(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end);
+
+    class ZipIterator {
+    public:
+        ZipIterator(Iterator it1, Iterator it2);
+        ZippedPair operator*() const;
+        bool operator!=(const ZipIterator& other);
+        void operator++();
+
+    private:
+        Iterator it_a_;
+        Iterator it_b_;
+    };
+    ZipIterator begin();  // NOLINT
+    ZipIterator end();    // NOLINT
+private:
+    Iterator a_begin_;
+    Iterator a_end_;
+    Iterator b_begin_;
+    Iterator b_end_;
+};
 
 Zipped Zip(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end);
